1f8705906b95fcb7c546808c87533576bf9d82c0
Paulo Victor Ribeiro Prediger <142063372+pauloprediger@users.noreply.github.com>
2024-10-04 23:16:31 -0300

    Projeto React Alura

---
+++ projeto_react_alura/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Banner from './components/Banner/Banner'
+
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <Banner/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ projeto_react_alura/src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+      <header className='banner'>
+            <img src = "/images/banner.png" alt='Organograma principal'/>
+      </header>
+  )
+}
+
+export default Banner
